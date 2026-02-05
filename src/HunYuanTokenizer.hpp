7d50ae8d86e4b9bcc6286b7b46a88a48319769c3
yongqiang <baiyongqiang@axera-tech.com>
2026-02-05 16:11:37 +0800

    add HunYuanTokenizer

---
+++ src/HunYuanTokenizer.hpp
@@ -0,0 +1,77 @@
+#pragma once
+#include <sstream>
+
+#include "BaseMixinTokenizer.hpp"
+#include "utils/object_register.hpp"
+#include "utils/sample_log.h"
+
+class HunYuanTokenizer : public BaseMixinTokenizer<TEXT>
+{
+protected:
+    bool add_generation_prompt = true;
+
+    const std::string bos_token = "<｜hy_begin▁of▁sentence｜>";
+    const std::string user_token = "<｜hy_User｜>";
+    const std::string assistant_token = "<｜hy_Assistant｜>";
+    const std::string placeholder_2 = "<｜hy_place▁holder▁no▁2｜>";
+    const std::string placeholder_3 = "<｜hy_place▁holder▁no▁3｜>";
+    const std::string placeholder_8 = "<｜hy_place▁holder▁no▁8｜>";
+
+public:
+    void set_add_generation_prompt(bool enable) override
+    {
+        add_generation_prompt = enable;
+    }
+
+    std::string apply_chat_template(const std::vector<Content> &contents) override
+    {
+        // check contents type
+        for (const auto &content : contents)
+        {
+            if (!this->support(content.type))
+            {
+                ALOGE("unsupport content type: %d", content.type);
+                return {};
+            }
+        }
+
+        std::stringstream text;
+        size_t start_index = 0;
+
+        if (!contents.empty() && contents[0].role == SYSTEM)
+        {
+            text << bos_token << contents[0].data << placeholder_3;
+            start_index = 1;
+        }
+        else
+        {
+            text << bos_token;
+        }
+
+        for (size_t i = start_index; i < contents.size(); ++i)
+        {
+            const auto &content = contents[i];
+            if (content.role == USER)
+            {
+                text << user_token << content.data;
+            }
+            else if (content.role == ASSISTANT)
+            {
+                text << assistant_token << content.data << placeholder_2;
+            }
+        }
+
+        if (!contents.empty() && contents.back().role == USER && add_generation_prompt)
+        {
+            text << assistant_token;
+        }
+        else
+        {
+            text << placeholder_8;
+        }
+
+        return text.str();
+    }
+};
+
+REGISTER(HunYuan, HunYuanTokenizer)
